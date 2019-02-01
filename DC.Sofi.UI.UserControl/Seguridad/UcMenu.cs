6fa5286320c3c34b0ef983f62950dc71d283c96f
Darling Chavez <darling.chavez@hotmail.com>
2019-01-31 21:57:11 -0500

    fix focus menu

--- DC.Sofi.UI.UserControl/Seguridad/UcMenu.cs
+++ DC.Sofi.UI.UserControl/Seguridad/UcMenu.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using DC.Sofi.Domain.Seguridad;
 using DevExpress.XtraTreeList;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace DC.Sofi.UI.UserControl.Seguridad
 {
@@ -12,8 +13,13 @@ namespace DC.Sofi.UI.UserControl.Seguridad
         public UcMenu()
         {
             InitializeComponent();
+            //// Disable the default look-and-feel settings. 
+            //this.treeListMenu.LookAndFeel.UseDefaultLookAndFeel = false;
+            //// Specify the skin. 
+            //this.treeListMenu.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.Skin;
+            //this.treeListMenu.LookAndFeel.SkinName = "Seven Classic";
         }
-
+        
         private DevExpress.XtraTreeList.Nodes.TreeListNode lastNodeSelected;
         public delegate void DoubleClickDelegate(object tag);
         public event DoubleClickDelegate DoubleClickEvent;
@@ -44,13 +50,15 @@ namespace DC.Sofi.UI.UserControl.Seguridad
             if (e.Node == lista.FocusedNode)
             {
                 e.Graphics.FillRectangle(SystemBrushes.Window, e.Bounds);
-                Rectangle rectangulo = new Rectangle(e.EditViewInfo.ContentRect.Left,
-                    e.EditViewInfo.ContentRect.Top,
+                var left = e.EditViewInfo.ContentRect.Left;
+                Rectangle rectanguloTexto = new Rectangle(left,e.EditViewInfo.ContentRect.Top,
                     Convert.ToInt32(e.Graphics.MeasureString(e.CellText, lista.Font).Width + 1),
                     Convert.ToInt32(e.Graphics.MeasureString(e.CellText, lista.Font).Height));
-                e.Graphics.FillRectangle(SystemBrushes.Highlight, rectangulo);
-                e.Graphics.DrawString(e.CellText, lista.Font, SystemBrushes.HighlightText, rectangulo);
 
+                Rectangle rectanguloBase = new Rectangle(left-3, 0,lista.Width,150);
+
+                e.Graphics.FillRectangle(SystemBrushes.Highlight, rectanguloBase);
+                e.Graphics.DrawString(e.CellText, lista.Font, SystemBrushes.HighlightText, rectanguloTexto);
                 e.Handled = true;
             }
         }
@@ -62,5 +70,6 @@ namespace DC.Sofi.UI.UserControl.Seguridad
                 this.treeListMenu.ExpandAll();
             }
         }
+        
     }
 }
